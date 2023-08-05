4ba62cbb808784d343fe8e39d842d27179686d73
Eelco <eelco@nhtcu.nl>
2023-08-05 10:50:14 +0200

    Added a basic test script

---
+++ tests/run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is made to run from the ./src directory
+
+# We need the directory of this script to find the test files
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "Test the creation of PDF files from RM files"
+for FILE in $SCRIPT_DIR/rm/*.rm; do
+  python3 -m rmc.cli -t pdf $FILE > /dev/null
+  FAILED=$?
+  if [ $FAILED -eq 1 ]; then
+      echo "TEST FAILED"
+      echo "python3 -m rmc.cli -t pdf $FILE > /dev/null"
+      exit
+  fi
+done
