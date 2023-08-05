93ec05f409c62a51f7fcb2c697f09a2f8383685b
Eelco <eelco@nhtcu.nl>
2023-08-05 11:03:08 +0200

    a bit more defensive markdown conversion

--- src/rmc/exporters/markdown.py
+++ src/rmc/exporters/markdown.py
@@ -12,23 +12,28 @@ def formatted_lines(doc):
 # From rmscene tests: test_text_files.py (modfied version extract_doc)
 def extract_doc(fh):
     tree = read_tree(fh)
-    assert tree.root_text
-    doc = TextDocument.from_scene_item(tree.root_text)
-    return doc
+    if tree.root_text:
+        assert tree.root_text
+        TextDocument.from_scene_item(tree.root_text)
+    else:
+        return None
 
 def print_text(fin, fout):
+
     doc = extract_doc(fin)
-    
-    for fmt, line in formatted_lines(doc):
-        if fmt == ParagraphStyle.BULLET:
-            print("- " + line, file=fout)
-        elif fmt == ParagraphStyle.BULLET2:
-            print("  + " + line, file=fout)
-        elif fmt == ParagraphStyle.BOLD:
-            print("> " + line, file=fout)
-        elif fmt == ParagraphStyle.HEADING:
-            print("# " + line, file=fout)
-        elif fmt == ParagraphStyle.PLAIN:
-            print(line, file=fout)
-        else:
-            print(("[unknown format %s] " % fmt) + line, file=fout)
+    if doc is  None:
+        print("No text content found")
+    else:
+        for fmt, line in formatted_lines(doc):
+            if fmt == ParagraphStyle.BULLET:
+                print("- " + line, file=fout)
+            elif fmt == ParagraphStyle.BULLET2:
+                print("  + " + line, file=fout)
+            elif fmt == ParagraphStyle.BOLD:
+                print("> " + line, file=fout)
+            elif fmt == ParagraphStyle.HEADING:
+                print("# " + line, file=fout)
+            elif fmt == ParagraphStyle.PLAIN:
+                print(line, file=fout)
+            else:
+                print(("[unknown format %s] " % fmt) + line, file=fout)
