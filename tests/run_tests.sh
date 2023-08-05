ab7aa7b3bbbbff372640dae6be6bca781de31fad
Eelco <eelco@nhtcu.nl>
2023-08-05 11:03:37 +0200

    Added more tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,6 +6,29 @@
 # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+
+echo "Test the creation of Markdown files from RM files"
+for FILE in $SCRIPT_DIR/rm/*.rm; do
+  python3 -m rmc.cli -t markdown $FILE > /dev/null
+  FAILED=$?
+  if [ $FAILED -eq 1 ]; then
+      echo "TEST FAILED"
+      echo "python3 -m rmc.cli -t markdown $FILE > /dev/null"
+      exit
+  fi
+done
+
+echo "Test the creation of SVG files from RM files"
+for FILE in $SCRIPT_DIR/rm/*.rm; do
+  python3 -m rmc.cli -t svg $FILE > /dev/null
+  FAILED=$?
+  if [ $FAILED -eq 1 ]; then
+      echo "TEST FAILED"
+      echo "python3 -m rmc.cli -t svg $FILE > /dev/null"
+      exit
+  fi
+done
+
 echo "Test the creation of PDF files from RM files"
 for FILE in $SCRIPT_DIR/rm/*.rm; do
   python3 -m rmc.cli -t pdf $FILE > /dev/null
@@ -16,3 +39,4 @@ for FILE in $SCRIPT_DIR/rm/*.rm; do
       exit
   fi
 done
+
